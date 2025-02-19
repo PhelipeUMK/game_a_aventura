3742d3c56ea59f7661a69dfb5cfdd17b44be2d86
PhelipeUMK <phelipe97ads@gmail.com>
2025-02-19 16:05:24 -0300

    Atualizei as animações
    Atualizei as animações de correr e pular do protagonista.

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var is_jumping := false
+
+@onready var anim :=$animation as AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -13,13 +16,22 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		is_jumping = true
+	elif is_on_floor():
+		is_jumping = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		anim.scale.x = direction
+		if !is_jumping:
+			anim.play("run")
+	elif is_jumping:
+		anim.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		anim.play("idle")
 
 	move_and_slide()
